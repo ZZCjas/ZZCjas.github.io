3b51135421268c92adeb31c76f3cfbda0fe2a6db
赵紫辰 <97828093+ZZCjas@users.noreply.github.com>
2023-07-31 23:34:43 +0800

    Add files via upload

---
+++ Brainfuck.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string s;
+int ptr=50000,top=0;
+int paper[100001];
+int st[100001];
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>s;
+	for(int i=0;i<s.size();i++)
+	{
+		if(!top&&s[i]==']')
+		{
+			cout<<"Runtime Error"<<endl;
+			return 0;
+		}
+		if(s[i]=='+')
+		{
+			paper[ptr]++;
+		}
+		else if(s[i]=='-')
+		{
+			paper[ptr]--;
+		}
+		else if(s[i]=='[')
+		{
+			st[++top]=i;
+		}
+		else if(s[i]==']')
+		{
+			if(paper[ptr])
+			{
+				i=st[top];
+			}
+			if(!paper[ptr])
+			{
+				top--;
+			}
+		}
+		else if(s[i]=='>')
+		{
+			ptr++;
+			if(ptr>=100000)
+			{
+				cout<<"Runtime Error\n";
+				return 0;
+			}
+		}
+		else if(s[i]=='<')
+		{
+			ptr--;
+			if(ptr==0)
+			{
+				cout<<"Runtime Error\n";
+				return 0;
+			}
+		}
+		else if(s[i]==',')
+		{
+			paper[ptr]=cin.get();
+		}
+		else if(s[i]=='.')
+		{
+			cout<<char(paper[ptr]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return 0;
+}
